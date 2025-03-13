0cb3c112af648c78373550f52588b80ccdfaae99
Maciej Dymek <mdymek.dy@gmail.com>
2025-03-13 08:56:03 +0100

    lekcja 13.03

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
-    path("admin/", admin.site.urls),
+    path('', views.landing_page, name="landing_page"),
+     path('admin/', admin.site.urls),
+     path("polls/", include("polls.urls")),
+     path('polls/', include("polls.urls")),
 ]
